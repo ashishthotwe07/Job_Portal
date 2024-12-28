6eb5ee09561ad6831e3b009d32317afe5318132c
Ashking@01 <ashishthotwe20@gmail.com>
2024-12-28 12:06:53 +0530

    backend -implement user functionalities for job applications and management

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node";
-import connectCloudinary from './config/cloudinary.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 // Initialize Express
@@ -22,13 +23,14 @@ app.use(cors())
 app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => res.send("API Working"));
+app.get('/', (req, res) => res.send("API Working"))
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
-app.post('/webhooks', clerkWebhooks);
-app.use('/api/company', companyRoutes);
-app.use('/api/jobs', jobRoutes);
+app.post('/webhooks', clerkWebhooks)
+app.use('/api/company', companyRoutes)
+app.use('/api/jobs', jobRoutes)
+app.use('/api/users', userRoutes)
 
 // Port
 const PORT = process.env.PORT || 5000
