9fe4bdf141989cefd624649cfe0ea5b2650ac498
Ashking@01 <ashishthotwe20@gmail.com>
2024-12-28 08:38:17 +0530

    setup Sentry for application error tracking and monitoring

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
+import * as Sentry from "@sentry/node";
 import connectCloudinary from './config/cloudinary.js'
 
 
@@ -18,10 +20,14 @@ app.use(express.json())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"));
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
 // Port
 const PORT = process.env.PORT || 5000
 
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
