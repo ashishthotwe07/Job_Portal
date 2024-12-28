130e9d1a7b4d4d13cd6cf0e88db39a224acb9aef
Ashking@01 <ashishthotwe20@gmail.com>
2024-12-28 14:40:56 +0530

    Fronted - added hero section and also recruiter login , setup context to fetch api's

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-
+import { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import 'quill/dist/quill.snow.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import RecruiterLogin from './components/RecruiterLogin';
+import { AppContext } from './context/AppContext';
 
 const App = () => {
 
-
+  const { showRecruiterLogin } = useContext(AppContext)
   return (
     <div>
-      
+       {showRecruiterLogin && <RecruiterLogin />}
       <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
