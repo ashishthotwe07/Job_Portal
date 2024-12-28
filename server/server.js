a6fe0606e046cdbd1b9629608bf5cc92c5680845
Ashking@01 <ashishthotwe20@gmail.com>
2024-12-28 13:10:08 +0530

    backend - Added Clerk middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 
 // Initialize Express
@@ -21,6 +22,7 @@ await connectCloudinary()
 // Middlewares
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
